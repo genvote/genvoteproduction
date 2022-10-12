c88d2cbb6f66f1ef6d8e0e9537d03c7c41f4c580
sbenson7 <scott.benson96@gmail.com>
2022-10-11 20:30:15 -0400

    Start events/ board edits

--- js/custom.js
+++ js/custom.js
@@ -12,7 +12,7 @@
 
     var nav_offset_top = $('.header_area').height() + 50;
     /*-------------------------------------------------------------------------------
-	  Navbar 
+      Navbar 
     -------------------------------------------------------------------------------*/
 
     //* Navbar Fixed  
@@ -130,7 +130,38 @@
     $('select').niceSelect();
 
     /*----------------------------------------------------*/
-
+      function active_course() {
+    if ($(".active_course").length) {
+      $(".active_course").owlCarousel({
+        loop: true,
+        margin: 20,
+        items: 3,
+        nav: true,
+        autoplay: 2500,
+        smartSpeed: 1500,
+        dots: false,
+        responsiveClass: true,
+        thumbs: true,
+        thumbsPrerendered: true,
+        navText: ["<img src='https://colorlib.com/preview/theme/edustage/img/prev.png'>", "<img src='https://colorlib.com/preview/theme/edustage/img/next.png'>"],
+        responsive: {
+          0: {
+            items: 1,
+            margin: 0
+          },
+          991: {
+            items: 2,
+            margin: 30
+          },
+          1200: {
+            items: 3,
+            margin: 30
+          }
+        }
+      });
+    }
+  }
+  active_course();
 
     /*-------------------
         Hero Slider
