f6af113fa72e47c715f2edd34188c4292bb6ac19
cmriter <99681954+cmriter@users.noreply.github.com>
2024-04-07 16:40:04 -0400

    GenVote Electoral Justice Summit

---
+++ js/obfuscatedPheonix.js
@@ -0,0 +1,16 @@
+function _0x5440(_0x22849b,_0x272d73){const _0x48811e=_0x4881();return _0x5440=function(_0x544048,_0x9b8667){_0x544048=_0x544048-0x14c;let _0x5f0193=_0x48811e[_0x544048];return _0x5f0193;},_0x5440(_0x22849b,_0x272d73);}function _0x4881(){const _0x3d4b40=['1770610OttEsZ','6cwNAWH','electoral-justice-summit.html','10cBeuDc','href','8RYQZoo','3xDwmMa','getElementById','533607XcMJOS','4955481EzZkrs','2551122gJpSZb','11685421XQVBKy','286070UoBfbe','password','Incorrect\x20password.\x20Please\x20try\x20again.','value','400520SAQdeU'];_0x4881=function(){return _0x3d4b40;};return _0x4881();}(function(_0x49afd0,_0x595703){const _0x4114f4=_0x5440,_0x22a2aa=_0x49afd0();while(!![]){try{const _0x335bca=parseInt(_0x4114f4(0x152))/0x1+parseInt(_0x4114f4(0x156))/0x2*(parseInt(_0x4114f4(0x150))/0x3)+parseInt(_0x4114f4(0x15a))/0x4+-parseInt(_0x4114f4(0x15b))/0x5*(parseInt(_0x4114f4(0x15c))/0x6)+parseInt(_0x4114f4(0x154))/0x7+-parseInt(_0x4114f4(0x14f))/0x8*(-parseInt(_0x4114f4(0x153))/0x9)+-parseInt(_0x4114f4(0x14d))/0xa*(parseInt(_0x4114f4(0x155))/0xb);if(_0x335bca===_0x595703)break;else _0x22a2aa['push'](_0x22a2aa['shift']());}catch(_0x22a4b7){_0x22a2aa['push'](_0x22a2aa['shift']());}}}(_0x4881,0x433c2));function checkPassword(){const _0x2c5d82=_0x5440,_0x292aac=document[_0x2c5d82(0x151)](_0x2c5d82(0x157))[_0x2c5d82(0x159)],_0x21d9f3=atob('cGhlb25peDI0');return _0x292aac===_0x21d9f3?(window['location'][_0x2c5d82(0x14e)]=_0x2c5d82(0x14c),![]):(alert(_0x2c5d82(0x158)),![]);}/*
+function checkPassword() {
+	const enteredPassword = document.getElementById('password').value;
+		
+	// Password is in base64 and converted to ASCII, currently "pheonix24"
+	const correctPassword = atob("cGhlb25peDI0");
+				
+	if (enteredPassword === correctPassword) {
+		window.location.href = "electoral-justice-summit.html";
+		return false; // prevent form submission
+	} else {
+		alert("Incorrect password. Please try again.");
+		return false; // prevent form submission
+	}
+}
+*/
